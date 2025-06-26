45fdcc2c9db14615d5384812a64535c577cc292b
NonStop1488 <andriytes99@gmail.com>
2025-06-27 02:40:04 +0300

    ідключення

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.post('/book', async (req, res) => {
   try {
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
